9fca148e23aa6a7d10458e8a051b51a1c325378d
Vladyslav Zuiev <vladyslav.zuiev@bshg.com>
2021-08-15 20:56:42 +0200

    DB-2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
+const testRouter = require("./route");
 
 const server = express();
 const port = process.env.PORT || 8000;
@@ -9,6 +10,7 @@ const port = process.env.PORT || 8000;
 server.use(cors());
 server.use(express.json());
 
+server.use("/test", testRouter);
 server.get("/", (req, res) => {
   res.send("<h1>This is a test application</h1>");
 });
---
+++ mode.js
@@ -0,0 +1,9 @@
+const db = require("./data/db-config");
+
+const findAll = () => {
+  return db("profiles");
+};
+
+module.exports = {
+  findAll,
+};
